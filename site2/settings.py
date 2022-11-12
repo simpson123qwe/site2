208581efe40273530bdd2b01353f095dafc69aa1
Your Name <you@example.com>
2022-11-12 11:52:05 +0300

    №1.0

--- site2/settings.py
+++ site2/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'site2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
